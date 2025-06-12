b6662e93eaa3ba73e1acf879a7715cc44b4dc6c4
RayCate1 <rcate3@vols.utk.edu>
2025-06-12 10:39:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
-st.title("Heightmap Actuator Extractor & 3D Curves")
+st.title("Heightmap Actuator Extractor & 3D Curves m ")
 
 # ── 1) MODEL INPUT ─────────────────────────────────────────
 uploaded = st.file_uploader("Upload planar geometry (OBJ/STL in mm)", type=["stl","obj"])
