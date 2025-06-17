80b383364d058717dcefb610b0313500cd706326
RayCate1 <rcate3@vols.utk.edu>
2025-06-17 15:55:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -267,4 +267,4 @@ if st.button("Process"):
         height=600, margin=dict(l=20, r=20, t=40, b=20)
     )
     st.plotly_chart(fig3d, use_container_width=True)
-    st.subheader("")
+    st.subheader("Rcate3@vols.utk.edu")
